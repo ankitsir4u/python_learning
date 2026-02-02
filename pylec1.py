8d18eb51181e5f5d5d3500c0283f2bd011c2e2fc
ankitsir4u <ankitsir4u@gmail.com>
2026-02-03 01:20:05 +0530

    m

---
+++ pylec1.py
@@ -0,0 +1,79 @@
+print("i am ankit.", "i am 20 years old.")
+print(34)
+print(23+32)
+
+
+name="ankit"
+age=24
+price =23.44
+age2 =age
+
+print("my name is :",name)
+print("my age is :",age2)
+
+print (type(name))
+print(type(age))
+print(type(price))
+
+a=2
+b=2
+sum=a+b
+print(sum)
+
+
+print("god")
+print("ankit")
+print("ankit")
+print("ankit")
+
+#arithmetic operators
+a=7
+b=5
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b)# a power b 
+
+#relational operator
+a=4
+b=3
+print(a==b)
+print(a!=b)
+print(a>=b)
+print(a>b)
+print(a<=b)
+print(a<b)
+
+#assignment operator
+
+num=10
+num *=10
+print("num:",num)
+
+
+#logical operators
+a=5
+b=8
+
+print(not False)
+print(not (b>a))
+
+val1 = True 
+val2 = False
+print  ("AND operator:",val1 and val2)
+print("OR operator:",val1 or val2)
+
+#type  casting
+a=int ("2")
+b=4.23
+
+
+print(type(a))
+print(a+b)
+
+#input
+
+name=input("enter your name:")
+print("welcome",name)
